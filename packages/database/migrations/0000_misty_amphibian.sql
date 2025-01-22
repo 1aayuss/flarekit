e12b1e65452b6e764b8475b69084f6ecb767690c
1aayuss <ayush.makwana700@gmail.com>
2025-01-23 00:02:31 +0530

    delete another .sql file

--- packages/database/migrations/0000_misty_amphibian.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `todos` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`title` text NOT NULL,
-	`description` text NOT NULL,
-	`created_at` text NOT NULL,
-	`completed` integer DEFAULT false NOT NULL
-);
